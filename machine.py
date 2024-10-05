d1dd8ca02c67a4ff0bfccdfbcb4a85bc22648374
Eryk Borycki <jaki.borycki@gmail.com>
2024-10-05 19:16:44 +0200

    WPISUJESZ KTORY COMMIT

--- machine.py
+++ machine.py
@@ -1,53 +1,72 @@
-from setting import *
+from player import Player
 from reel import *
+from setting import *
+from win import *
 import pygame
 
+
 class Machine:
-        # WYSWIETLANIE SAMEGO EKRANU
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.reel_list = {}  # Tworzy pusty słownik, który będzie przechowywać bębny (reels)
-        self.reel_index = 0 # Inicjalizuje indeks bębna, zaczynając od 0
+        self.machine_balance = 10000.00
+        self.reel_index = 0
+        self.reel_list = {}
         self.can_toggle = True
         self.spinning = False
+        self.can_animate = False
+        self.win_animation_ongoing = False
 
-        self.spawn_reels() # Wywołuje metodę, która generuje i wyświetla bębny
+        # Results
+        self.prev_result = {0: None, 1: None, 2: None, 3: None, 4: None}
+        self.spin_result = {0: None, 1: None, 2: None, 3: None, 4: None}
+
+        self.spawn_reels()
+        self.currPlayer = Player()
+
 
     def cooldowns(self):
+        # Only lets player spin if all reels are NOT spinning
         for reel in self.reel_list:
             if self.reel_list[reel].reel_is_spinning:
                 self.can_toggle = False
-                self.spinning = False
+                self.spinning = True
 
         if not self.can_toggle and [self.reel_list[reel].reel_is_spinning for reel in self.reel_list].count(False) == 5:
             self.can_toggle = True
+            self.spin_result = self.get_result()
 
+            if self.check_wins(self.spin_result):
+                self.win_data = self.check_wins(self.spin_result)
+                # Play the win sound
+                # self.play_win_sound(self.win_data)
+                self.pay_player(self.win_data, self.currPlayer)
+                #self.win_animation_ongoing = True
+                print(self.currPlayer.get_data())
 
     def input(self):
         keys = pygame.key.get_pressed()
 
-        #if keys[pygame.K_SPACE] and self.can_toggle and self.currPlayer.balance >= self.currPlayer.bet_size:
-        if keys[pygame.K_SPACE]:
+        # Checks for space key, ability to toggle spin, and balance to cover bet size
+        if keys[pygame.K_SPACE] and self.can_toggle and self.currPlayer.balance >= self.currPlayer.bet_size:
             self.toggle_spinning()
             self.spin_time = pygame.time.get_ticks()
-            ##self.currPlayer.place_bet()
-            ##self.machine_balance += self.currPlayer.bet_size
-            ##self.currPlayer.last_payout = None
+            self.currPlayer.place_bet()
+            self.machine_balance += self.currPlayer.bet_size
+            self.currPlayer.last_payout = None
 
-    def draw_reels(self,delta_time):
+    def draw_reels(self, delta_time):
         for reel in self.reel_list:
             self.reel_list[reel].animate(delta_time)
 
-    def spawn_reels(self): # FUNKCJA DO WYSWIETLANIA ICH NA EKRANIE
-        if not self.reel_list: # Sprawdza, czy lista bębnów jest pusta
-            x_topleft, y_topleft = 10, -300 # Ustawia początkową pozycję pierwszego bębna na ekranie (10, -300)
-        while self.reel_index < 5:  # Pętla działa, dopóki nie zostanie utworzone 5 bębnów
-            if self.reel_index > 0:  # Jeśli to nie pierwszy bęben
-                x_topleft, y_topleft = x_topleft + (300 + X_OFFSET), y_topleft  # Przesuwa bęben w poziomie o 300 pikseli + wartość X_OFFSET
+    def spawn_reels(self):
+        if not self.reel_list:
+            x_topleft, y_topleft = 10, -300
+        while self.reel_index < 5:
+            if self.reel_index > 0:
+                x_topleft, y_topleft = x_topleft + (300 + X_OFFSET), y_topleft
 
-            self.reel_list[self.reel_index] = Reel(
-                (x_topleft, y_topleft))  # Tworzy nowy bęben i dodaje go do listy bębnów pod odpowiednim indeksem
-            self.reel_index += 1  # Zwiększa indeks bębna o 1, aby przygotować miejsce na kolejny
+            self.reel_list[self.reel_index] = Reel((x_topleft, y_topleft))  # Need to create reel class
+            self.reel_index += 1
 
     def toggle_spinning(self):
         if self.can_toggle:
@@ -56,13 +75,57 @@ class Machine:
             self.can_toggle = False
 
             for reel in self.reel_list:
-                self.reel_list[reel].start_spin(int(reel)*200)
+                self.reel_list[reel].start_spin(int(reel) * 200)
+                # self.spin_sound.play()
+                self.win_animation_ongoing = False
 
+    def get_result(self):
+        for reel in self.reel_list:
+            self.spin_result[reel] = self.reel_list[reel].reel_spin_result()
+        return self.spin_result
 
-    def update(self, delta_time):  # Funkcja aktualizująca stan bębnów w czasie gry
+    def check_wins(self, result):
+        hits = {}
+        horizontal = flip_horizontal(result)
+        for row in horizontal:
+            for sym in row:
+                if row.count(sym) > 2:  # Potential win
+                    possible_win = [idx for idx, val in enumerate(row) if sym == val]
+
+                    # Check possible_win for a subsequence longer than 2 and add to hits
+                    if len(longest_seq(possible_win)) > 2:
+                        hits[horizontal.index(row) + 1] = [sym, longest_seq(possible_win)]
+        if hits:
+            self.can_animate = True
+            return hits
+
+    def pay_player(self, win_data, curr_player):
+        multiplier = 0
+        spin_payout = 0
+        for v in win_data.values():
+            multiplier += len(v)
+            print(v)
+        spin_payout = (multiplier * curr_player.bet_size)
+        curr_player.balance += spin_payout
+        self.machine_balance -= spin_payout
+        curr_player.last_payout = spin_payout
+        curr_player.total_won += spin_payout
+
+    # You need to provide sounds and load them in the Machine init function for this to work!
+
+    def update(self, delta_time):
+        self.cooldowns()
         self.input()
         self.draw_reels(delta_time)
-        for reel in self.reel_list:  # Iteruje przez wszystkie bębny w liście bębnów
-            self.reel_list[reel].symbol_list.draw(self.display_surface)  # Rysuje symbole na ekranie (display_surface) dla każdego bębna
-            self.reel_list[reel].symbol_list.update()  # Aktualizuje stan każdego symbolu na bębnie (np. animacje, pozycje)
+        for reel in self.reel_list:
+            self.reel_list[reel].symbol_list.draw(self.display_surface)
+            self.reel_list[reel].symbol_list.update()
 
+        # Balance/payout debugger
+        # debug_player_data = self.currPlayer.get_data()
+        # machine_balance = "{:.2f}".format(self.machine_balance)
+        # if self.currPlayer.last_payout:
+        #     last_payout = "{:.2f}".format(self.currPlayer.last_payout)
+        # else:
+        #     last_payout = "N/A"
+        # debug(f"Player balance: {debug_player_data['balance']} | Machine balance: {machine_balance} | Last payout: {last_payout}")
