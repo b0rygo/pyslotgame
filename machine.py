c0f0e2192cd616fecdb9d29e584a746b93b3c35f
Eryk Borycki <jaki.borycki@gmail.com>
2024-10-05 13:46:46 +0200

    Fifth commit

--- machine.py
+++ machine.py
@@ -3,24 +3,66 @@ from reel import *
 import pygame
 
 class Machine:
+        # WYSWIETLANIE SAMEGO EKRANU
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.reel_list = {}
-        self.reel_index = 0
+        self.reel_list = {}  # Tworzy pusty słownik, który będzie przechowywać bębny (reels)
+        self.reel_index = 0 # Inicjalizuje indeks bębna, zaczynając od 0
+        self.can_toggle = True
+        self.spinning = False
 
-        self.spawn_reels()
+        self.spawn_reels() # Wywołuje metodę, która generuje i wyświetla bębny
 
-    def spawn_reels(self):
-        if not self.reel_list:
-            x_topleft, y_topleft = 10, -300
-        while self.reel_index < 5:
-            if self.reel_index > 0:
-                x_topleft, y_topleft = x_topleft + (300 + X_OFFSET), y_topleft
-
-            self.reel_list[self.reel_index] = Reel((x_topleft, y_topleft))  # Need to create reel class
-            self.reel_index += 1
-
-    def update(self, delta_time):
+    def cooldowns(self):
         for reel in self.reel_list:
-            self.reel_list[reel].symbol_list.draw(self.display_surface)
-            self.reel_list[reel].symbol_list.update()
+            if self.reel_list[reel].reel_is_spinning:
+                self.can_toggle = False
+                self.spinning = False
+
+        if not self.can_toggle and [self.reel_list[reel].reel_is_spinning for reel in self.reel_list].count(False) == 5:
+            self.can_toggle = True
+
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        #if keys[pygame.K_SPACE] and self.can_toggle and self.currPlayer.balance >= self.currPlayer.bet_size:
+        if keys[pygame.K_SPACE]:
+            self.toggle_spinning()
+            self.spin_time = pygame.time.get_ticks()
+            ##self.currPlayer.place_bet()
+            ##self.machine_balance += self.currPlayer.bet_size
+            ##self.currPlayer.last_payout = None
+
+    def draw_reels(self,delta_time):
+        for reel in self.reel_list:
+            self.reel_list[reel].animate(delta_time)
+
+    def spawn_reels(self): # FUNKCJA DO WYSWIETLANIA ICH NA EKRANIE
+        if not self.reel_list: # Sprawdza, czy lista bębnów jest pusta
+            x_topleft, y_topleft = 10, -300 # Ustawia początkową pozycję pierwszego bębna na ekranie (10, -300)
+        while self.reel_index < 5:  # Pętla działa, dopóki nie zostanie utworzone 5 bębnów
+            if self.reel_index > 0:  # Jeśli to nie pierwszy bęben
+                x_topleft, y_topleft = x_topleft + (300 + X_OFFSET), y_topleft  # Przesuwa bęben w poziomie o 300 pikseli + wartość X_OFFSET
+
+            self.reel_list[self.reel_index] = Reel(
+                (x_topleft, y_topleft))  # Tworzy nowy bęben i dodaje go do listy bębnów pod odpowiednim indeksem
+            self.reel_index += 1  # Zwiększa indeks bębna o 1, aby przygotować miejsce na kolejny
+
+    def toggle_spinning(self):
+        if self.can_toggle:
+            self.spin_time = pygame.time.get_ticks()
+            self.spinning = not self.spinning
+            self.can_toggle = False
+
+            for reel in self.reel_list:
+                self.reel_list[reel].start_spin(int(reel)*200)
+
+
+    def update(self, delta_time):  # Funkcja aktualizująca stan bębnów w czasie gry
+        self.input()
+        self.draw_reels(delta_time)
+        for reel in self.reel_list:  # Iteruje przez wszystkie bębny w liście bębnów
+            self.reel_list[reel].symbol_list.draw(self.display_surface)  # Rysuje symbole na ekranie (display_surface) dla każdego bębna
+            self.reel_list[reel].symbol_list.update()  # Aktualizuje stan każdego symbolu na bębnie (np. animacje, pozycje)
+
--- reel.py
+++ reel.py
@@ -2,31 +2,61 @@ from setting import *
 import pygame, random
 
 class Reel:
-    def __init__(self,pos):
-        self.symbol_list = pygame.sprite.Group()
-        self.shuffled_keys = list(symbols.keys())
-        random.shuffle(self.shuffled_keys)
-        self.shuffled_keys = self.shuffled_keys[:5]
+    def __init__(self, pos):  # Konstruktor klasy Reel, przyjmujący argument `pos`, czyli pozycję na ekranie
+        self.symbol_list = pygame.sprite.Group()  # Tworzy grupę (listę) symboli (używając Pygame, aby później móc nimi zarządzać)
+        self.shuffled_keys = list(symbols.keys())  # Pobiera wszystkie klucze (nazwy symboli) ze słownika `symbols` i zamienia je na listę
+        random.shuffle(self.shuffled_keys)  # Tasuje listę kluczy, aby symbole pojawiały się w losowej kolejności
+        self.shuffled_keys = self.shuffled_keys[:5]  # Ogranicza liczbę symboli na bębnie do 5 (pierwsze 5 elementów)
 
         self.reel_is_spinning = False
 
-        for idx, item in enumerate(self.shuffled_keys):
-            self.symbol_list.add(Symbol(symbols[item],pos,idx))
-            pos = list(pos)
-            pos[1] += 300
-            pos = tuple(pos)
+        for idx, item in enumerate(self.shuffled_keys):  # Iteruje przez wylosowane symbole, przypisując im indeksy
+            self.symbol_list.add(Symbol(symbols[item], pos, idx))  # Dodaje nowy obiekt Symbol do listy symboli, przekazując ścieżkę do pliku, pozycję i indeks
+            pos = list(pos)  # Zamienia krotkę `pos` na listę (aby była modyfikowalna)
+            pos[1] += 300  # Przesuwa każdy kolejny symbol w dół o 300 pikseli
+            pos = tuple(pos)  # Ponownie zamienia `pos` na krotkę (bo Pygame wymaga krotek dla pozycji)
 
-class Symbol(pygame.sprite.Sprite):
-    def __init__(self, pathToFile, pos, idx):
-        super().__init__()
+    def animate(self, delta_time):
+        if self.reel_is_spinning:
+            self.delay_time -= (delta_time * 1000)
+            self.spin_time -= (delta_time * 1000)
+            reel_is_stopping  = False
 
-        self.sym_type = pathToFile.split('/')[0]
+            if self.spin_time < 0:
+                reel_is_stopping = True
 
-        self.pos = pos
-        self.idx = idx
-        self.image = pygame.image.load(pathToFile).convert_alpha()
-        self.rect  = self.image.get_rect(topleft = pos)
-        self.x_val = self.rect.left
+            if self.delay_time <= 0:
+
+                for symbol in self.symbol_list:
+                    symbol.rect.bottom += 100
+
+                    #
+                    if symbol.rect.top == 1200:
+                        if reel_is_stopping:
+                            self.reel_is_spinning = False
+
+                        symbol_idx = symbol.idx
+                        symbol.kill()
+                        self.symbol_list.add(Symbol(symbols[random.choice(self.shuffled_keys)],((symbol.x_val), -300), symbol_idx))
+
+    def start_spin(self, delay_time):
+        self.delay_time = delay_time
+        self.spin_time = 1000 + delay_time
+        self.reel_is_spinning = True
+
+
+
+class Symbol(pygame.sprite.Sprite):  # Definicja klasy Symbol, dziedziczącej po klasie pygame.sprite.Sprite
+    def __init__(self, pathToFile, pos, idx):  # Konstruktor klasy Symbol, przyjmujący ścieżkę do pliku, pozycję i indeks
+        super().__init__()  # Wywołanie konstruktora klasy bazowej (pygame.sprite.Sprite)
+
+        self.sym_type = pathToFile.split('/')[0]  # Rozdziela ścieżkę do pliku po znakach '/', aby uzyskać nazwę typu symbolu (tu pierwszy element)
+
+        self.pos = pos  # Przechowuje pozycję symbolu
+        self.idx = idx  # Przechowuje indeks symbolu (numer kolejności na bębnie)
+        self.image = pygame.image.load(pathToFile).convert_alpha()  # Ładuje obraz symbolu z pliku i konwertuje go na format z przezroczystością (alpha)
+        self.rect = self.image.get_rect(topleft=pos)  # Tworzy prostokąt (rect) z pozycją obrazu na ekranie (topleft określa górny-lewy róg)
+        self.x_val = self.rect.left  # Przechowuje wartość pozycji poziomej (x) lewego boku symbolu
 
     def update(self):
         pass
